9ebe0d95fea7b11a6c86f15fb16829780c7fd66f
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-05-16 17:33:39 -0300

    feat: Added to automatically find the door.

--- Controle Bancada/app.py
+++ Controle Bancada/app.py
@@ -91,4 +91,5 @@ def api_alterar_senha():
     return jsonify({"status": "sucesso"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=porta)
