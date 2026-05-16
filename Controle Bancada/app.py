6ddab913fbc5cac87238af446a66be8b8c7a3071
DalvanGustavo <dalvan.oliveira2004@gmail.com>
2026-05-16 18:00:33 -0300

    refactor:aligned command with ESP32

--- Controle Bancada/app.py
+++ Controle Bancada/app.py
@@ -51,7 +51,7 @@ def api_ligar():
     segundos = dados.get('segundos')
     print(f"[COMANDO] Ligar relé por {minutos} min e {segundos} seg.")
     # No futuro, aqui vai o código MQTT que envia a mensagem pro ESP32
-    mqtt_client.publish(MQTT_TOPIC, f"ligar:{minutos}:{segundos}")
+    mqtt_client.publish(MQTT_TOPIC, f"LIGAR {minutos} {segundos}")
     return jsonify({"status": "ligado"})
 
 @app.route('/api/desligar', methods=['POST'])
@@ -61,7 +61,7 @@ def api_desligar():
     
     print("[COMANDO] Desligar relé.")
     # No futuro, aqui vai o código MQTT que manda o ESP32 parar
-    mqtt_client.publish(MQTT_TOPIC, "desligar")
+    mqtt_client.publish(MQTT_TOPIC, "DESLIGAR")
     return jsonify({"status": "desligado"})
 
 @app.route('/api/bloquear', methods=['POST'])
